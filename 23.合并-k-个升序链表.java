702583c66601c951c68616e8745f29a45cad1ca4
WhiteBlacken <2059164874@qq.com>
2023-08-01 20:26:33 +0800

    合并数组/链表相关

--- 23.合并-k-个升序链表.java
+++ 23.合并-k-个升序链表.java
@@ -5,20 +5,37 @@
  */
 
 // @lc code=start
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b)->a.val-b.val);
+        for(ListNode list:lists){
+            if(list!=null)minHeap.offer(list);
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while(!minHeap.isEmpty()){ //listnode为null，则不加入minheap.
+            ListNode node = minHeap.poll();
+            cur.next = node;
+            cur = cur.next;
+            if(cur.next!=null){
+                minHeap.offer(cur.next);
+            }
+        }
+        return dummy.next;
     }
 }
 // @lc code=end
-
